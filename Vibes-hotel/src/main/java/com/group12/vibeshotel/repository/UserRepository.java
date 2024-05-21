d5ab09781f7575786ab60c2b4237130f4b966050
Nguyễn Anh Thư <156945289+nganhthu@users.noreply.github.com>
2024-05-21 11:23:21 +0700

    Update UserRepository.java

--- Vibes-hotel/src/main/java/com/group12/vibeshotel/repository/UserRepository.java
+++ Vibes-hotel/src/main/java/com/group12/vibeshotel/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 /**
- * @author Simpson Alfred
+ * @author nganhthu
  */
 
 public interface UserRepository extends JpaRepository<User, Long> {
